d649b7d60a5aa638df8448c3cee6a02595612023
Leonardo M <lmoreno@tese.edu.mx>
2019-08-28 08:48:21 -0500

    Crecion de la pagina de super heroes con despliegue de lista y estilo en el componente

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr. Strange' },
+  { id: 12, name: 'Hulk' },
+  { id: 13, name: 'Thor' },
+  { id: 14, name: 'SpiderMan' },
+  { id: 15, name: 'Wolverine' },
+  { id: 16, name: 'Capitan America' },
+  { id: 17, name: 'Capitena Marvel' },
+  { id: 18, name: 'Batman' },
+  { id: 19, name: 'SuperMan' },
+  { id: 20, name: 'BlackWidow' }
+];
